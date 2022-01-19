edb02c35af078639b4a84f52987ca654263c3ce5
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-01-19 09:55:25 +0000

    Form auto backup script's commit

--- helpers.py
+++ helpers.py
@@ -149,3 +149,63 @@ def evaluate_correspondence(img_A, img_B, ground_truth_correspondence_file,
 
     # x1, y1, x2, y2 = scio.loadmat(eval_file)
     x1 = file_contents['x1']
+    y1 = file_contents['y1']
+    x2 = file_contents['x2']
+    y2 = file_contents['y2']
+
+    pointsA = np.zeros((len(x1), 2))
+    pointsB = np.zeros((len(x2), 2))
+
+    for i in range(len(x1)):
+        pointsA[i, 0] = x1[i]
+        pointsA[i, 1] = y1[i]
+        pointsB[i, 0] = x2[i]
+        pointsB[i, 1] = y2[i]
+
+    correct_matches = 0
+
+    F = estimate_fundamental_matrix(pointsA, pointsB)
+    top50 = 0
+    top100 = 0
+
+    for i in range(x1_matches.shape[0]):
+        pointA = np.ones((1, 3))
+        pointB = np.ones((1, 3))
+        pointA[0, 0] = x1_matches[i]
+        pointA[0, 1] = y1_matches[i]
+        pointB[0, 0] = x2_matches[i]
+        pointB[0, 1] = y2_matches[i]
+
+        if abs(pointB @ F @ np.transpose(pointA)) < .1:
+            x_dists = x1 - x1_matches[i]
+            y_dists = y1 - y1_matches[i]
+
+            # computes distances of each interest point to the ground truth point
+            dists = np.sqrt(np.power(x_dists, 2.0) + np.power(y_dists, 2.0))
+            closest_ground_truth = np.argmin(dists, axis=0)
+            offset_x1 = x1_matches[i] - x1[closest_ground_truth]
+            offset_y1 = y1_matches[i] - y1[closest_ground_truth]
+            offset_x1 *= img_B.shape[0] / img_A.shape[0]
+            offset_y1 *= img_B.shape[0] / img_A.shape[0]
+            offset_x2 = x2_matches[i] - x2[closest_ground_truth]
+            offset_y2 = y2_matches[i] - y2[closest_ground_truth]
+            offset_dist = np.sqrt(np.power(offset_x1 - offset_x2, 2) + np.power(offset_y1 - offset_y2, 2))
+            if offset_dist < 70:
+                correct_matches += 1
+                good_matches[i] = True
+        if i == 49:
+            print(f'Accuracy on 50 most confident: {int(100 * correct_matches / 50)}%')
+            top50 = correct_matches
+        if i == 99:
+            print(f'Accuracy on 100 most confident: {int(100 * correct_matches / 100)}%')
+            top100 = correct_matches
+
+    print(f'Accuracy on all matches: {int(100 * correct_matches / len(matches))}%')
+
+    if vis > 0:
+        print("Vizualizing...")
+        visualize.show_correspondences(img_A, img_B, x1_est / scale_factor, y1_est / scale_factor,
+                                       x2_est / scale_factor, y2_est / scale_factor, matches, good_matches, vis,
+                                       filename)
+
+    return top50, top100, correct_matches
