5c3e73817c0880ad8f0e714b85a763a3f4015506
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-01-19 20:21:20 +0000

    .ignore

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 # Local Feature Stencil Code
 # Written by James Hays for CS 143 @ Brown / CS 4476/6476 @ Georgia Tech with Henry Hu <henryhu@gatech.edu>
+# Edited by James Tompkin
+# Adapted for python by asabel and jdemari1 (2019)
