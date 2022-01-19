e5935ed53153ff849e3c77dec0d79d0452ca61e9
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-01-19 11:46:52 +0000

    v0.11.117.00+101

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 # Local Feature Stencil Code
-# Written by James Hays for CS 143 @ Brown / CS 
+# Written by James Hays for CS 143 @ Brown / CS 4476/6476 @ Georgia Tech with Henry Hu <henryhu@gatech.edu>
