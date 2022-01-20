1324dd615f716f9c067446690e48e4aefabef85d
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-01-20 13:27:06 +0000

    New translations 03_p01_ch03_02.md (Tagalog)

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # Written by James Hays for CS 143 @ Brown / CS 4476/6476 @ Georgia Tech with Henry Hu <henryhu@gatech.edu>
 # Edited by James Tompkin
 # Adapted for python by asabel and jdemari1 (2019)
+
+import argparse
+
+import matplotlib
+import matplotlib.pyplot as p
