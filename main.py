dda7cb4103344f14d27da73e597faaa8f90418f2
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-04-19 19:53:40 +0000

    Integration Orange3-Shadow

--- main.py
+++ main.py
@@ -116,4 +116,8 @@ def main():
     # make images smaller to speed up the algorithm. This parameter
     # gets passed into the evaluation code, so don't resize the images
     # except for changing this parameter - We will evaluate your code using
-    # scale_factor = 0.5, so be aware of 
+    # scale_factor = 0.5, so be aware of this
+    scale_factor = 0.5
+
+    # Bilinear rescaling
+ 
