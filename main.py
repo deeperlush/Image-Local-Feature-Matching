ffd748a71aee87069bbdf2bf2938080ec6def25a
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-05-01 12:16:19 +0000

    Update equation src URL

--- main.py
+++ main.py
@@ -136,4 +136,5 @@ def main():
 
     # For development and debugging get_features and match_features, you will likely
     # want to use the ta ground truth points, you can comment out the preceding two
-    # lines and uncomment the following 
+    # lines and uncomment the following line to do this. Note that the ground truth
+    # points 
