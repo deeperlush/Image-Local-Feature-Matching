2ba345cde2ff0781a6d7a8f8dd9c68b3bb686bb4
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-06-03 14:48:41 +0000

    added more info

--- memusecheck.py
+++ memusecheck.py
@@ -1,4 +1,10 @@
 import memory_profiler
 from skimage import io, img_as_float32
 from skimage.color import rgb2gray
-from skimage.transform import re
+from skimage.transform import rescale
+
+import student
+from helpers import evaluate_correspondence
+
+
+def mem
