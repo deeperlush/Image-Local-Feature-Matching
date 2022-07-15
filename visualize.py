87e52873fe13281352ee7bf66aa8258d4a816a5d
deeperlush <157482200+deeperlush@users.noreply.github.com>
2022-07-15 15:11:51 +0000

    form work

--- visualize.py
+++ visualize.py
@@ -5,4 +5,6 @@ import numpy as np
 from skimage.feature import plot_matches
 
 
-def show_correspondences(imgA, imgB, X1, Y1, X2, Y2, matches, good_matches, n
+def show_correspondences(imgA, imgB, X1, Y1, X2, Y2, matches, good_matches, number_to_display, filename=None):
+	"""
+		Visualizes corresponding point
